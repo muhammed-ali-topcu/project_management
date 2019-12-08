0f41f89db08f4a288feada73187c9229c6f056b1
Muhammed Ali <m.alitopcu77@gmail.com>
2019-12-08 12:22:47 +0300

    localization

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,7 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
+Route::get('/locale',function(){
+    App::setlocale('en');
+    return __('text.welcome',['name'=>'MuhammedAli']);
+});
